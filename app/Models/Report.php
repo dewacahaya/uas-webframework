cfaa3325077618f2659825e33632f0b68c53fba6
dewacahaya <dewacahaya30@gmail.com>
2024-12-21 21:45:28 +0800

    + orders page (mvc), report page (mvc) - controller get data dari customer untuk order & report

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -15,4 +15,25 @@ class Report extends Model
     {
         return $this->belongsTo(Busana::class, 'busana_id');
     }
+
+    public static function updateFromOrders($bulan, $tahun)
+    {
+        // Ambil data order sesuai bulan dan tahun
+        $orderDetails = OrderDetail::whereHas('order', function ($query) use ($bulan, $tahun) {
+            $query->whereMonth('tanggal_pesan', $bulan)
+                ->whereYear('tanggal_pesan', $tahun);
+        })->get();
+
+        $groupedDetails = $orderDetails->groupBy('busana_id');
+
+        foreach ($groupedDetails as $busanaId => $details) {
+            $totalPesanan = $details->sum('jumlah');
+            $totalPenjualan = $details->sum(fn($detail) => $detail->jumlah * $detail->harga);
+
+            $report = self::updateOrCreate(
+                ['busana_id' => $busanaId, 'bulan' => $bulan, 'tahun' => $tahun],
+                ['total_pesanan' => $totalPesanan, 'total_penjualan' => $totalPenjualan]
+            );
+        }
+    }
 }
